4fdf3271d8d6d0222223ecd6e2609cebfb7cc55f
Tribalize <christoddsmith@gmail.com>
2026-09-15 16:13:57 -0400

    test

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,14 @@ include(":bootstrap-geyser")
 project(":bootstrap-standalone").projectDir = file("bootstrap/standalone")
 project(":bootstrap-geyser").projectDir = file("bootstrap/geyser")
 
+includeBuild("vendor/cloudburst-protocol") {
+    dependencySubstitution {
+        substitute(module("org.cloudburstmc.protocol:common")).using(project(":common"))
+        substitute(module("org.cloudburstmc.protocol:bedrock-codec")).using(project(":bedrock-codec"))
+        substitute(module("org.cloudburstmc.protocol:bedrock-connection")).using(project(":bedrock-connection"))
+    }
+}
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
